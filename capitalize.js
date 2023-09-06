bc2f71ccb825b9e1a80249788793d9022e382b07
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-07 00:02:58 +0800

    Update to work with nested arrays

--- capitalize.js
+++ capitalize.js
@@ -1,9 +1,5 @@
 export default function capitalize(str){
-    if(Array.isArray(str)){
-        return str.map(item => {
-            if(typeof item === "string") return capitalize(item);
-        })
-    }
+    if(Array.isArray(str)) return str.map(item => capitalize(item));
     if(typeof str !== "string") return str;
     const split = str.split(" ");
     if(split.length > 1) return split.map(word => word.slice(0,1).toUpperCase() + word.slice(1)).join(" ");
