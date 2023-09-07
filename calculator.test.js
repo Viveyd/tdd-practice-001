728148d49769c71b8e94e8ccf79acc69100e3e7c
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-07 14:21:10 +0800

    Add test cases

---
+++ calculator.test.js
@@ -0,0 +1,15 @@
+it("Return sum of a & b", () => {
+    expect(calculator.add(5,3)).toBe(8);
+})
+
+it("Return diff of a & b", () => {
+    expect(calculator.subtract(5,3)).toBe(2);
+})
+
+it("Return product of a & b", () => {
+    expect(calculator.add(6,3)).toBe(18);
+})
+
+it("Return quotient of a & b", () => {
+    expect(calculator.add(6,3)).toBe(2);
+})
