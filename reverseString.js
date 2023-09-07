a51a1eea02483cc4319775bbb0f7d16474e20618
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-07 10:25:23 +0800

    Fn can now reverse arrays, even nested

--- reverseString.js
+++ reverseString.js
@@ -1,4 +1,5 @@
 export default function reverseString(str){
+    if(Array.isArray(str)) return str.map(item => reverseString(item)).reverse().join("");
     if(typeof str !== "string") return str;
     return str.split("").reverse().join("");
 }
