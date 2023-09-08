4855d436c8e33ebc0df4525fc5ec9c1417d40a9c
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-08 09:14:34 +0800

    Add uppercase test cases and update description

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,9 +1,18 @@
 import caesarCipher from "./caesarCipher"
 
-it("Test 1", () => {
+it("(a,1) => b", () => {
     expect(caesarCipher("a", 1)).toBe("b");
 })
 
-it("Test 2", () => {
+it("(z, 1) => a", () => {
     expect(caesarCipher("z", 1)).toBe("a");
 })
+
+it("(A,1) => B", () => {
+    expect(caesarCipher("A", 1)).toBe("B");
+})
+
+it("(Z,1) => A", () => {
+    expect(caesarCipher("Z", 1)).toBe("A");
+})
+
