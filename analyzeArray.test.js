0c5dba01167e282622541c606eec0df6d24d553d
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-08 10:47:31 +0800

    Fn returns min & max (w/ test case)

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,5 +1,13 @@
 import analyzeArray from "./analyzeArray"
 
-it('Returns average', () => {
+it('Returns .average', () => {
     expect(analyzeArray([1,2,3]).average).toBe(2)
+})
+
+it('Returns .min', () => {
+    expect(analyzeArray([1,2,3]).min).toBe(1)
+})
+
+it('Returns .max', () => {
+    expect(analyzeArray([1,2,3]).max).toBe(3)
 })
