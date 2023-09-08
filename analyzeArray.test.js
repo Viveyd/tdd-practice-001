437b4454e997e0238dae3fb926189b55a8f2bd5b
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-08 10:49:36 +0800

    Fn returns len (w/ tests)

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,13 +1,17 @@
 import analyzeArray from "./analyzeArray"
 
 it('Returns .average', () => {
-    expect(analyzeArray([1,2,3]).average).toBe(2)
+    expect(analyzeArray([1,2,3]).average).toBe(2);
 })
 
 it('Returns .min', () => {
-    expect(analyzeArray([1,2,3]).min).toBe(1)
+    expect(analyzeArray([1,2,3]).min).toBe(1);
 })
 
 it('Returns .max', () => {
-    expect(analyzeArray([1,2,3]).max).toBe(3)
+    expect(analyzeArray([1,2,3]).max).toBe(3);
+})
+
+it('Returns .len', () => {
+    expect(analyzeArray([1,2,3]).len).toBe(3);
 })
