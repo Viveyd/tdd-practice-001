c797b4a00a59b993556cc89eb56dba32a4ff0edb
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-08 09:12:35 +0800

    Cipher works with single alphabet char

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,10 @@
 export default function caesarCipher(string, shifts){
-    return
+    if(typeof string !== "string") return string;
+    let charCode = string.charCodeAt(0);
+    if(charCode >= 97 && charCode <= 122){
+        charCode += shifts;
+        while(charCode > 122) charCode = 96 + (charCode - 122);
+    }
+    else return string;
+    return String.fromCharCode(charCode);
 }
