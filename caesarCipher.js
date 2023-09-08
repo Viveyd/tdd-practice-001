4b7c5d3580ce27b1afd653e23aa5538d5f3f246c
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-08 09:39:14 +0800

    Cipher works with multi-char

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,12 +1,16 @@
 export default function caesarCipher(string, shifts){
     if(typeof string !== "string") return string;
-    let charCode = string.charCodeAt(0);
-    if(charCode >= 97 && charCode <= 122){
-        charCode += shifts;
-        while(charCode > 122) charCode = 96 + (charCode - 122);
-    } else if (charCode >= 65 && charCode <= 90){
-        charCode += shifts;
-        while(charCode > 90) charCode = 64 + (charCode - 90);
-    } else return string;
-    return String.fromCharCode(charCode);
+    let ciphered = "";
+    for(let i = 0; i < string.length; i++){
+        let charCode = string.charCodeAt(i);
+        if(charCode >= 97 && charCode <= 122){
+            charCode += shifts;
+            while(charCode > 122) charCode = 96 + (charCode - 122);
+        } else if (charCode >= 65 && charCode <= 90){
+            charCode += shifts;
+            while(charCode > 90) charCode = 64 + (charCode - 90);
+        }
+        ciphered += String.fromCharCode(charCode);
+    }
+    return ciphered;
 }
