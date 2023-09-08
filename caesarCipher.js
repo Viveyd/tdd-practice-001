418efc62eb69696397c78dd4174240c1c1df7199
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-08 09:16:56 +0800

    Cipher works with uppercase

--- caesarCipher.js
+++ caesarCipher.js
@@ -4,7 +4,9 @@ export default function caesarCipher(string, shifts){
     if(charCode >= 97 && charCode <= 122){
         charCode += shifts;
         while(charCode > 122) charCode = 96 + (charCode - 122);
-    }
-    else return string;
+    } else if (charCode >= 65 && charCode <= 90){
+        charCode += shifts;
+        while(charCode > 90) charCode = 64 + (charCode - 90);
+    } else return string;
     return String.fromCharCode(charCode);
 }
